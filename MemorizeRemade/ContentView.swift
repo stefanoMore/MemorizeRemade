06727b873bf5028081ce56fd320f08bcb1465697
Stefano Morelli <108464272+stefanoMore@users.noreply.github.com>
2023-11-21 13:17:24 +0100

    Initial Commit

---
+++ MemorizeRemade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizeRemade
+//
+//  Created by Stefano Morelli on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizeRemade/MemorizeRemadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeRemadeApp.swift
+//  MemorizeRemade
+//
+//  Created by Stefano Morelli on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeRemadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
